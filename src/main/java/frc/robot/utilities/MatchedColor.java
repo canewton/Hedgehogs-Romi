85dc13dc9489314a1e72a61fd1a2d1512d8dc35a
1canewton <1canewton@seattleschools.org>
2021-08-25 01:22:33 -0700

    Initial Commit

--- src/main/java/frc/robot/utilities/MatchedColor.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.utilities;
-
-/**
- * Enumerator for control panel colors. 
- * Unifies ColorMatcher and FMS color data. 
- */
-public enum MatchedColor {
-    RED, BLUE, GREEN, YELLOW, UNKNOWN;
-}
